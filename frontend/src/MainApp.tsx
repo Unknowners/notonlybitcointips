953cecf3f420f8fdeccabc933a588717761d3be7
Vlad Bocharov <devbocharov@gmail.com>
2025-08-08 03:40:29 +0300

    v0.5.1: Update app name from Donation Hub to Not Only Bitcoin Tips
    - Change footer copyright text to use correct app name
    - Update version to 0.5.1

--- frontend/src/MainApp.tsx
+++ frontend/src/MainApp.tsx
@@ -582,9 +582,9 @@ export default function MainApp() {
         )}
       </div>
       <div className="mt-8 text-gray-400 text-xs text-center select-none">
-        &copy; {new Date().getFullYear()} Donation Hub. Powered by ICP Hackathon.
+        &copy; {new Date().getFullYear()} Not Only Bitcoin Tips. Powered by ICP Hackathon.
         <br />
-        Version 0.5.0
+        Version 0.5.1
       </div>
     </div>
   );
